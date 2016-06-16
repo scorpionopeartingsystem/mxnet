304d4265a1687ecd2e5491d6ad0bb771eb62d495
Liang Xiang <xlvector@gmail.com>
2016-06-16 11:07:35 +0800

    price

--- example/price/data.py
+++ example/price/data.py
@@ -19,7 +19,37 @@ def fetch(sql):
         print e
     return res
 
-car_source = fetch("SELECT a.price, a.deal_price, a.create_time, a.car_id, a.minor_category_name, a.license_date, a.license_month, a.road_haul, a.city_id, b.pinpai, b.chexi, b.chexing, b.guobie, b.zhidaojiage, b.niankuan, b.cheliangleixing, b.cheliangjibie, b.biansuxiangleixing, b.cheshenxingshi FROM ganji_vehicle.vehicle_c2c_car_source a join ganji_vehicle.liyang_car b where a.car_id = b.id;")
+car_source = fetch("SELECT a.price, a.deal_price, a.suggest_price, a.evaluator, a.create_time, a.car_id, a.minor_category_name, a.license_date, a.license_month, a.road_haul, a.city_id, b.pinpai, b.chexi, b.chexing, b.guobie, b.zhidaojiage, b.niankuan, b.cheliangleixing, b.cheliangjibie, b.biansuxiangleixing, b.cheshenxingshi FROM ganji_vehicle.vehicle_c2c_car_source a join ganji_vehicle.liyang_car b where a.car_id = b.id;")
 
+good_evaluators = set()
+for line in file("./data/good_evaluator.tsv"):
+    line = line.strip()
+    if len(line) > 0:
+        good_evaluators.add(line.strip())
+
+sw_price = open("./data/price.tsv", "w")
+sw_deal = open("./data/price_deal.tsv", "w")
+sw_eval = open("./data/price_eval.tsv", "w")
 for row in car_source:
-    print '\t'.join([str(x).strip() for x in row])
+    row = [str(x).strip() for x in row]
+    price, deal_price, suggest_price, evaluator = row[:4]
+    others = row[4:]
+    sw_price.write('\t'.join([price] + others) + '\n')
+    
+    price = float(price)
+    deal_price = float(deal_price)
+    suggest_price = float(suggest_price)
+    
+    deal_ratio = min(deal_price, price) / (1.0 + max(deal_price, price))
+    if deal_price > 0 and deal_ratio > 0.7:
+        sw_deal.write('\t'.join([str(deal_price)] + others) + '\n')
+
+    if evaluator in good_evaluators:
+        suggest_ratio = min(suggest_price, price) / (1.0 + max(suggest_price, price))
+        if suggest_price > 0 and suggest_ratio > 0.7:
+            sw_eval.write('\t'.join([str(suggest_price)] + others) + '\n')
+
+sw_price.close()
+sw_deal.close()
+sw_eval.close()
+
--- example/price/train_price.py
+++ example/price/train_price.py
@@ -22,17 +22,12 @@ class Batch(object):
         return [(n, x.shape) for n, x in zip(self.label_names, self.label)]
 
 def process_line(line):
-    price, deal_price, create_time, car_id, minor_category_name, \
+    price, create_time, car_id, minor_category_name, \
         license_date, license_month, road_haul, city_id, \
         pinpai, chexi, chexing, guobie, zhidaojiage, \
         niankuan, cheliangleixing, cheliangjibie, biansuxiangleixing, \
         cheshenxingshi = line.strip('\n').split('\t')
-    deal_price = float(deal_price)
     price = float(price)
-    ratio = min(price, deal_price) / max(price, deal_price)
-    price = 0.0
-    if deal_price > 0. and ratio > 0.75:
-        price = deal_price
     date_delta = datetime.datetime.fromtimestamp(int(create_time)).date() - datetime.date(int(license_date),int(license_month),15)
     vehicle_age = date_delta.days
     pinpai = pinpai.replace(" ", "").replace(":", "")
@@ -50,27 +45,11 @@ def process_line(line):
         zhidaojiage_max = float(zhidaojiage_tks[-1])
     except Exception, e:
         pass
-    return price / 10000.0, vehicle_age, 'pp_' + pinpai, 'chexi_' + chexi, 'chexing_' + chexing, road_haul, city_id, \
-        zhidaojiage_min, zhidaojiage_max, 'gb_' + guobie, 'nk_' + niankuan, 'cllx_' + cheliangleixing, 'cljb_' + cheliangjibie, 'bsxlx_' + biansuxiangleixing, 'csxs_' + cheshenxingshi
-
-def interval(price):
-    if price < 5:
-        return 0
-    elif price >= 5 and price < 10:
-        return 1
-    elif price >= 10 and price < 15:
-        return 2
-    elif price >= 15 and price < 20:
-        return 3
-    elif price >= 20 and price < 30:
-        return 4
-    elif price >= 30 and price < 40:
-        return 5
-    elif price >= 40 and price < 60:
-        return 6
-    else:
-        return 7
-    
+    return price / 10000.0, vehicle_age, 'pp_' + pinpai, 'chexi_' + chexi, \
+        'chexing_' + chexing, road_haul, city_id, \
+        zhidaojiage_min, zhidaojiage_max, 'gb_' + guobie, \
+        'nk_' + niankuan, 'cllx_' + cheliangleixing, 'cljb_' + cheliangjibie, \
+        'bsxlx_' + biansuxiangleixing, 'csxs_' + cheshenxingshi
 
 def insert_vocab(a, b):
     if b not in a:
@@ -102,24 +81,26 @@ def save_vocab(vocab, fname):
     sw.close()
 
 class PriceIter(mx.io.DataIter):
-    def __init__(self, fname, batch_size, vchexing, vcity):
+    def __init__(self, fname, batch_size, vchexing, vcity, is_train):
         super(PriceIter, self).__init__()
         self.batch_size = batch_size
-        self.is_train = False
-        if fname.startswith("train"):
-            self.is_train = True
+        self.is_train = is_train
         self.city = []
         self.chexing = []
-        self.cdata = []
+        self.data = []
         self.chexing_vocab = vchexing
         self.city_vocab = vcity
         self.price = []
+        n = 0
         for line in file(fname):
+            n += 1
+            if is_train and n % 10 == 0: 
+                continue
+            if not is_train and n % 10 > 0:
+                continue
             price, vehicle_age, pinpai, chexi, chexing, road_haul, city_id, \
                 zhidaojiage_min, zhidaojiage_max, guobie, niankuan, cheliangleixing, cheliangjibie, biansuxiangleixing, cheshenxingshi = process_line(line)
-            if price <= 0.:
-                continue
-            self.cdata.append(np.array([vehicle_age / 10.0, float(road_haul)/ 10.0, 
+            self.data.append(np.array([vehicle_age / 10.0, float(road_haul)/ 10.0, 
                                         zhidaojiage_min / 10.0, zhidaojiage_max / 10.0]))
             self.chexing.append(np.array([self.chexing_vocab[pinpai], 
                                           self.chexing_vocab[chexi], 
@@ -135,36 +116,33 @@ class PriceIter(mx.io.DataIter):
             self.city.append(np.array([self.city_vocab[city_id]], dtype=np.int))
             self.price.append(price)
 
-        self.provide_data = [('cdata', (batch_size, 4)),
+        self.provide_data = [('data', (batch_size, 4)),
                              ('chexing', (batch_size, 9)),
                              ('city', (batch_size, 1))]
-        self.provide_label = [('price', (self.batch_size, )), ('price_interval', (self.batch_size,))]
+        self.provide_label = [('price', (self.batch_size, ))]
 
     def __iter__(self):
         count = len(self.price)
         for i in range(count / self.batch_size):
-            bcdata = []
+            bdata = []
             bcity = []
             bchexing = []
             blabel = []
             blabel_interval = []
             for j in range(self.batch_size):
                 k = (i * self.batch_size + j)
-                if self.is_train:
-                    k = random.randint(0, count - 1)
-                bcdata.append(self.cdata[k])
+                bdata.append(self.data[k])
                 bcity.append(self.city[k])
                 bchexing.append(self.chexing[k])
                 blabel.append(self.price[k])
-                blabel_interval.append(interval(self.price[k]))
                 
-            data_all = [mx.nd.array(bcdata),
+            data_all = [mx.nd.array(bdata),
                         mx.nd.array(bchexing),
                         mx.nd.array(bcity)
             ]
-            label_all = [mx.nd.array(blabel), mx.nd.array(blabel_interval)]
-            data_names = ['cdata', 'chexing', 'city']
-            label_names = ['price', 'price_interval']
+            label_all = [mx.nd.array(blabel)]
+            data_names = ['data', 'chexing', 'city']
+            label_names = ['price']
             
             data_batch = Batch(data_names, data_all, label_names, label_all)
             yield data_batch
@@ -174,13 +152,12 @@ class PriceIter(mx.io.DataIter):
 
 def get_net(chexing_size, chexing_embed_size,
             city_size, city_embed_size):
-    cdata = mx.symbol.Variable('cdata')
+    data = mx.symbol.Variable('data')
     price = mx.symbol.Variable('price')
-    price_interval = mx.symbol.Variable('price_interval')
     chexing = mx.symbol.Variable('chexing')
     city = mx.symbol.Variable('city')
 
-    cdata = mx.symbol.Flatten(data = cdata)
+    data = mx.symbol.Flatten(data = data)
     chexing_embed = mx.symbol.Embedding(data = chexing, 
                                         input_dim = chexing_size,
                                         output_dim = chexing_embed_size,
@@ -192,7 +169,7 @@ def get_net(chexing_size, chexing_embed_size,
                                      name="city_embed")
     city_flatten = mx.symbol.Flatten(city_embed)
 
-    concat = mx.symbol.Concat(*[cdata,
+    concat = mx.symbol.Concat(*[data,
                              chexing_flatten, city_flatten], dim = 1)
     rnet = mx.symbol.FullyConnected(data = concat, num_hidden = 100, name="rfc1")
     rnet = mx.symbol.Activation(data = rnet, act_type="relu")
@@ -201,14 +178,7 @@ def get_net(chexing_size, chexing_embed_size,
     rnet = mx.symbol.FullyConnected(data = rnet, num_hidden = 1, name = "rfc3")
     rnet = mx.symbol.Activation(data = rnet, act_type="relu")
     rnet = mx.symbol.LinearRegressionOutput(data = rnet, label = price, name="rnet")
-    
-    cnet = mx.symbol.FullyConnected(data = concat, num_hidden = 100, name="cfc1")
-    cnet = mx.symbol.Activation(data = cnet, act_type="relu")
-    cnet = mx.symbol.FullyConnected(data = cnet, num_hidden = 100, name="cfc2")
-    cnet = mx.symbol.Activation(data = cnet, act_type="relu")
-    cnet = mx.symbol.FullyConnected(data = cnet, num_hidden = 8, name="cfc3")
-    cnet = mx.symbol.SoftmaxOutput(data = cnet, label = price_interval, name="cnet")
-    return mx.symbol.Group([rnet, cnet])
+    return rnet
 
 def RMSE(label, pred):
     ret = 0.0
@@ -222,14 +192,16 @@ def RMSE(label, pred):
         n += 1.0
     return ret / n
 
-vchexing, vcity = build_vocab("price.tsv")
-save_vocab(vchexing, "chexing.vocab")
-save_vocab(vcity, "city.vocab")
+fname = sys.argv[1]
+vchexing, vcity = build_vocab("./data/price.tsv")
+save_vocab(vchexing, "./data/chexing.vocab")
+save_vocab(vcity, "./data/city.vocab")
 
-data_train = PriceIter("train.tsv", 100, vchexing, vcity)
-data_test = PriceIter("test.tsv", 100, vchexing, vcity)
+data_train = PriceIter("./data/" + fname + ".tsv", 32, vchexing, vcity, True)
+data_test = PriceIter("./data/" + fname + ".tsv", 32, vchexing, vcity, False)
 
-network = get_net(chexing_size = len(data_train.chexing_vocab), chexing_embed_size = 1000,
+network = get_net(chexing_size = len(data_train.chexing_vocab), 
+                  chexing_embed_size = 1000,
                   city_size = len(data_train.city_vocab), city_embed_size = 300)
 
 devs = [mx.gpu(i) for i in range(1)]
@@ -246,6 +218,8 @@ import logging
 head = '%(asctime)-15s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=head)
 
-model.fit(X = data_train, eval_data = data_test, eval_metric = mx.metric.np(RMSE), batch_end_callback=mx.callback.Speedometer(32, 50),)
+model.fit(X = data_train, eval_data = data_test, 
+          eval_metric = mx.metric.np(RMSE), 
+          batch_end_callback=mx.callback.Speedometer(32, 50),)
 
-model.save("deal-price")
+model.save("./data/" + fname)
